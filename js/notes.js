e460db40ae00d7d961befedf0c0b4d84c794c445
IBASjbv <134215845+IBASjbv@users.noreply.github.com>
2023-05-22 19:17:42 -0300

    Update notes.js

--- js/notes.js
+++ js/notes.js
@@ -1,7 +1,7 @@
 const btnSaveNotes = document.getElementById('btn-save-notes')
 const notes = document.getElementById('notes')
 
-notes.innerText = localStorage.getItem('notes')
+notes.value = localStorage.getItem('notes')
 
 btnSaveNotes.addEventListener('click', () => {
     const text = notes.value
